bc1a5eb9757a6cd86f70ae246b0624caf1210031
“bastienr17” <bastienroc@free.fr>
2023-03-23 18:12:50 +0100

    fix conflict

--- config/bundles.php
+++ config/bundles.php
@@ -9,6 +9,4 @@ return [
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
-    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
