071d26b5d6f9949007f153abb240fb7a09a2d924
“bastienr17” <bastienroc@free.fr>
2023-03-23 15:35:04 +0100

    fix  index :lipstick:

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,6 @@ return [
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
