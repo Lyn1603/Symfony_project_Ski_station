def513c32b8f37c078606ca6421cc5b71b8265cc
Thomas Doret-Gaïsset <doretg.t@gmail.com>
2023-03-24 12:29:14 +0100

    :art: update domaine crud controller

--- src/Controller/Admin/DomaineCrudController.php
+++ src/Controller/Admin/DomaineCrudController.php
@@ -4,6 +4,9 @@ namespace App\Controller\Admin;
 
 use App\Entity\Domaine;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class DomaineCrudController extends AbstractCrudController
 {
@@ -12,14 +15,17 @@ class DomaineCrudController extends AbstractCrudController
         return Domaine::class;
     }
 
-    /*
+    
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->hideOnForm(),
+            TextField::new('name'),
+	          ImageField::new('pictureUrl')
+		          ->setUploadDir('public/uploads/domaine')
+		          ->setBasePath('uploads/domaine')
+		          ->setUploadedFileNamePattern('[randomhash].[extension]'),
         ];
     }
-    */
+    
 }
