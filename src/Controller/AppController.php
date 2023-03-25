09a28731352636ad2efd6d42a108df2b35843817
Lynda Benabdessadok <lylyben74@gmail.com>
2023-03-26 00:21:03 +0100

    some push

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -60,7 +60,7 @@ class AppController extends AbstractController
     }
 
 
-    #[Route('/pistes', name: 'app_piste')]
+    #[Route('/{id}', name: 'app_piste')]
     public function pistes( PistesRepository $pistesRepository): Response
     {
         return $this->render('pistes/piste.html.twig', [
