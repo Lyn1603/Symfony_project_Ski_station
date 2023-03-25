430b07f2b9860fd42317d5030e26d8be619a3499
tinkode92 <yanis.bekrarchouche@edu.devinci.fr>
2023-03-25 14:21:49 +0100

    feat: order restaurants by rating

--- src/Twig/Runtime/RestaurantRuntime.php
+++ src/Twig/Runtime/RestaurantRuntime.php
@@ -3,7 +3,6 @@
 namespace App\Twig\Runtime;
 
 use App\Repository\PistesRepository;
-use Doctrine\Common\Collections\Collection;
 use Twig\Extension\RuntimeExtensionInterface;
 
 class RestaurantRuntime implements RuntimeExtensionInterface
@@ -15,8 +14,19 @@ class RestaurantRuntime implements RuntimeExtensionInterface
         $this->pistesRepository = $pistesRepository;
     }
 
-    public function getRestaurants(int $piste_id): array
-    {
-        return $this->pistesRepository->find($piste_id)->getRestaurants()->toArray();
+    public function getRestaurants(int $piste_id, array $query): array
+    {   
+        $order = isset($query['order']) ? $query['order'] : null;
+        $restaurants = $this->pistesRepository->find($piste_id)->getRestaurants()->toArray();
+        if ($order == 'asc' || $order == 'desc') {
+            $restaurants_stars = array();
+            foreach ($restaurants as $key => $restaurant)
+            {
+                $restaurants_stars[$key] = $restaurant->getStars();
+            }
+            array_multisort($restaurants_stars, $order == 'asc' ? SORT_ASC : SORT_DESC, $restaurants);
+        }
+
+        return $restaurants;
     }
 }
