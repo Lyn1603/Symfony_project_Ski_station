dc0d31e3e8cb6853521926f01de34a6469cd949e
tinkode92 <yanis.bekrarchouche@edu.devinci.fr>
2023-03-26 15:30:59 +0200

    fix: migrations: use mariadb

--- migrations/Version20230326132846.php
+++ migrations/Version20230326132846.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230324101447 extends AbstractMigration
+final class Version20230326132846 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -21,11 +21,15 @@ final class Version20230324101447 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE pistes (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, length DOUBLE PRECISION NOT NULL, open_at TIME NOT NULL, close_at TIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, firstname VARCHAR(255) NOT NULL, phone VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE restaurant ADD CONSTRAINT FK_EB95123FC34065BC FOREIGN KEY (piste_id) REFERENCES pistes (id)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE restaurant DROP FOREIGN KEY FK_EB95123FC34065BC');
         $this->addSql('DROP TABLE pistes');
+        $this->addSql('DROP TABLE user');
     }
 }
